0ecb5e0d1095c8eb8af4ab4e1ccb8ec56bd987b1
Juan B <jbenitez@fi.uba.ar>
2018-11-03 12:51:57 -0300

    Parcheado todos los inconvenientes del iterador

--- hash.c
+++ hash.c
@@ -95,6 +95,9 @@ long int hash_recorrer(const hash_t* hash, const char* clave){
 long int iter_hash_recorrer(hash_iter_t* iter, size_t inicio){
 
 	long int i;
+  if (iter->nodo_contador == iter->cantidad_final_hash){
+    return -1;
+  }
 	for(i = inicio; iter->tabla[i].estado != OCUPADO; i++);
 	return i;
 }
@@ -243,7 +246,7 @@ hash_iter_t *hash_iter_crear(const hash_t *hash){
 
 bool hash_iter_al_final(const hash_iter_t *iter){
 
-	if (iter->nodo_contador >= iter->cantidad_final_hash)
+	if (iter->nodo_contador > iter->cantidad_final_hash || iter->posicion == -1)
 		return true;
 	else
 		return false;
